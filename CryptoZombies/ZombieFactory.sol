cde9cdd949d5ca23f7f2882cae93877f753c156d
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2021-03-31 23:50:39 -0400

    CryptoZombies Chapter 8

--- CryptoZombies/ZombieFactory.sol
+++ CryptoZombies/ZombieFactory.sol
@@ -12,6 +12,8 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    // start here
+    function createZombie (string memory _name, uint _dna) public {
+        zombies.push(Zombie(_name, _dna));
+    }
 
 }
